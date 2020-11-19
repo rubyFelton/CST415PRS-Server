2d6d84f831ecfcdc2de2d6f34a60c3741e9d697d
Ruby Felton <ruby.felton@oit.edu>
2020-11-18 23:17:21 -0800

    Add project files.

---
+++ PRSServer/PRSServerProgram.cs
@@ -0,0 +1,340 @@
+﻿// PRSServerProgram.cs
+// Oregon Institute of Technology
+// Pete Myers
+// CST 415 Networks
+// Fall 2020
+// 
+//Ruby Felton
+//10/08/2020
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using PRSLib;
+
+namespace PRSServer
+{
+    class PRSServerProgram
+    {
+        class PRS
+        {
+            // represents a PRS Server, keeps all state and processes messages accordingly
+
+            class PortReservation
+            {
+                private ushort port;
+                private bool available;
+                private string serviceName;
+                private DateTime lastAlive;
+
+                public PortReservation(ushort port)
+                {
+                    this.port = port;
+                    available = true;
+                }
+
+                public string ServiceName { get { return serviceName; } }
+                public ushort Port { get { return port; } }
+                public bool Available { get { return available; } }
+
+                public bool Expired(int timeout)
+                { 
+                    // return true if timeout seconds have elapsed since lastAlive
+                    return DateTime.Now > lastAlive.AddSeconds(timeout);
+                }
+
+                public void Reserve(string serviceName)
+                {
+                    // reserve this port for serviceName
+                    available = false;
+                    this.serviceName = serviceName;
+                    lastAlive = DateTime.Now;
+                }
+
+                public void KeepAlive()
+                {
+                    // save current time in lastAlive
+                    lastAlive = DateTime.Now;
+                }
+
+                public void Close()
+                {
+                    // make this reservation available
+                    available = true;
+                    serviceName = null;
+                }
+            }
+
+            // server attribues
+            private ushort startingClientPort;
+            private ushort endingClientPort;
+            private int keepAliveTimeout;
+            private int numPorts;
+            private PortReservation[] ports;
+            private bool stopped;
+
+            public PRS(ushort startingClientPort, ushort endingClientPort, int keepAliveTimeout)
+            {
+
+                // save parameters
+                this.startingClientPort = startingClientPort;
+                this.endingClientPort = endingClientPort;
+                this.keepAliveTimeout = keepAliveTimeout;
+                
+                // initialize to not stopped
+                stopped = false;
+
+                // initialize port reservations
+                numPorts = endingClientPort - startingClientPort + 1; //inclusive
+                ports = new PortReservation[numPorts];
+
+                //loop through port res array, fill in port numbers
+                for (ushort port = startingClientPort; port <= endingClientPort; port++)
+                {
+                    //array is zero based, port numbers start at starting port
+                    ports[port - startingClientPort] = new PortReservation(port);
+                }
+            }
+
+            public bool Stopped { get { return stopped; } }
+
+            private void CheckForExpiredPorts()
+            {
+                // expire any ports that have not been kept alive with in time out
+                foreach (PortReservation reservation in ports)
+                {
+                    //if port is not available and is passed keep alive time out close it
+                    if (!reservation.Available && reservation.Expired(keepAliveTimeout))
+                    {
+                        reservation.Close();
+                    }
+                }
+            }
+
+            private PRSMessage RequestPort(string serviceName)
+            {
+                PRSMessage response = null;
+
+                if (ports.SingleOrDefault(p => p.ServiceName == serviceName && !p.Available) == null)
+                {
+                    // client has requested the lowest available port, so find it!
+                    PortReservation reservation = null;
+                    reservation = ports.FirstOrDefault(p => p.Available);
+
+                    // if found an avialable port, reserve it and send SUCCESS
+                    if (reservation != null)
+                    {
+                        reservation.Reserve(serviceName);
+                        response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, serviceName, reservation.Port, PRSMessage.STATUS.SUCCESS);
+                    }
+                    else
+                    {
+                        response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, serviceName, 0, PRSMessage.STATUS.ALL_PORTS_BUSY);
+
+                    }
+                }
+                else
+                {
+                    response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, serviceName, 0, PRSMessage.STATUS.SERVICE_IN_USE);
+                }
+
+                return response;
+            }
+
+            public PRSMessage HandleMessage(PRSMessage msg)
+            {
+                // handle one message and return a response
+
+                PRSMessage response = null;
+
+                //check for expired ports
+                CheckForExpiredPorts();
+
+                switch (msg.MsgType)
+                {
+                    case PRSMessage.MESSAGE_TYPE.REQUEST_PORT:
+                        {
+
+                            //try to reserve requested port and send requested port back in response
+                            response = RequestPort(msg.ServiceName);
+                        }
+                        break;
+
+                    case PRSMessage.MESSAGE_TYPE.KEEP_ALIVE:
+                        {
+                            // client has requested that we keep their port alive
+                            // find the reserved port by port# and service name
+                            PortReservation reservation = ports.FirstOrDefault(p => !p.Available && p.ServiceName == msg.ServiceName && p.Port == msg.Port);
+                            
+                            // if found, keep it alive and send SUCCESS else
+                            if (reservation !=null)
+                            {
+                                reservation.KeepAlive();
+                                response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, reservation.ServiceName, reservation.Port, PRSMessage.STATUS.SUCCESS);
+                            }
+                            else
+                            {
+                                response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, msg.ServiceName, 0, PRSMessage.STATUS.SERVICE_NOT_FOUND);
+                            }
+                        }
+                        break;
+
+                    case PRSMessage.MESSAGE_TYPE.CLOSE_PORT:
+                        {
+                            // client has requested that we close their port
+                            // find the reserved port by port# and service name
+                            PortReservation reservation = ports.FirstOrDefault(p => !p.Available && p.ServiceName == msg.ServiceName && p.Port == msg.Port);
+
+                            // if found, keep it alive and send SUCCESS else
+                              if (reservation != null)
+                            {
+                                reservation.Close();
+                                response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, msg.ServiceName, reservation.Port, PRSMessage.STATUS.SUCCESS);
+                            }
+                            else
+                            {
+                                response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, msg.ServiceName, msg.Port, PRSMessage.STATUS.SERVICE_NOT_FOUND);
+                            }
+                        }
+                        break;
+
+                    case PRSMessage.MESSAGE_TYPE.LOOKUP_PORT:
+                        {
+                            // client wants to know the reserved port number for a named service
+                            // find the port
+                            PortReservation reservation = ports.FirstOrDefault(p => !p.Available && p.ServiceName == msg.ServiceName);
+
+                            // if found, send port number back
+                            // else, SERVICE_NOT_FOUND
+                            if (reservation != null)
+                            {
+                                response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, msg.ServiceName, reservation.Port, PRSMessage.STATUS.SUCCESS);
+                            }
+                            else
+                            {
+                                response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, msg.ServiceName, msg.Port, PRSMessage.STATUS.SERVICE_NOT_FOUND);
+                            }
+
+                        }
+                        break;
+
+                    case PRSMessage.MESSAGE_TYPE.STOP:
+                        {
+                            // client is telling us to close the appliation down
+                            // stop the PRS and return SUCCESS
+                            stopped = true;
+                            response = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, "", 0, PRSMessage.STATUS.SUCCESS);
+                        }
+                        break;
+                }
+
+                return response;
+            }
+
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("usage: PRSServer [options]");
+            Console.WriteLine("\t-p < service port >");
+            Console.WriteLine("\t-s < starting client port number >");
+            Console.WriteLine("\t-e < ending client port number >");
+            Console.WriteLine("\t-t < keep alive time in seconds >");
+        }
+
+        static void Main(string[] args)
+        {
+
+            // defaults
+            ushort SERVER_PORT = 30000;
+            ushort STARTING_CLIENT_PORT = 40000;
+            ushort ENDING_CLIENT_PORT = 40099;
+            int KEEP_ALIVE_TIMEOUT = 10;
+
+            try
+            {
+                // process command options
+                // -p < service port >
+                // -s < starting client port number >
+                // -e < ending client port number >
+                // -t < keep alive time in seconds >
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "-p":
+                            SERVER_PORT = Convert.ToUInt16(args[++i]);
+                            break;
+                        case "-s":
+                            STARTING_CLIENT_PORT = Convert.ToUInt16(args[++i]);
+                            break;
+                        case "-e":
+                            ENDING_CLIENT_PORT = Convert.ToUInt16(args[++i]);
+                            break;
+                        case "-t":
+                            KEEP_ALIVE_TIMEOUT = Convert.ToInt16(args[++i]);
+                            break;
+                    }
+                }
+
+                if (STARTING_CLIENT_PORT <= SERVER_PORT || STARTING_CLIENT_PORT >= ENDING_CLIENT_PORT)
+                {
+                    throw new Exception("Invalid range: -p must be outside of -s to -e range and -e must be larger then -s");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Command line Arguments");
+                Console.WriteLine(ex);
+                Usage();
+            }
+       
+            
+            // initialize the PRS server
+            PRS prs = new PRS(STARTING_CLIENT_PORT, ENDING_CLIENT_PORT, KEEP_ALIVE_TIMEOUT);
+           
+            // create the socket for receiving messages at the server
+            Socket listeningSocket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+
+            // bind the listening socket to the PRS server port
+            listeningSocket.Bind(new IPEndPoint(IPAddress.Any, SERVER_PORT));
+            
+            // Process client messages
+
+            while (!prs.Stopped)
+            {
+                EndPoint clientEndPoint = null;
+                try
+                {
+                    // receive a message from a client
+                    clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    PRSMessage msg = PRSMessage.ReceiveMessage(listeningSocket, ref clientEndPoint);
+                    
+                    // let the PRS handle the message
+                    PRSMessage responMessage = prs.HandleMessage(msg);
+
+                    // send response message back to client
+                    responMessage.SendMessage(listeningSocket, clientEndPoint);
+                }
+                catch (Exception ex)
+                {
+                    // attempt to send a UNDEFINED_ERROR response to the client, if we know who that was
+                    if (clientEndPoint != null)
+                    {
+                        PRSMessage errorMessage = new PRSMessage(PRSMessage.MESSAGE_TYPE.RESPONSE, "", 0, PRSMessage.STATUS.UNDEFINED_ERROR );
+                    }
+                }
+            }
+
+            // close the listening socket
+            listeningSocket.Close();
+
+            // wait for a keypress from the user before closing the console window
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PRSTestClient/PRSTestClientProgram.cs
@@ -0,0 +1,245 @@
+﻿// PRSTestClientProgram.cs
+// Oregon Institute of Technology
+// Pete Myers
+// CST 415 Networks
+// Fall 2020
+// 
+//Ruby Felton
+//10/08/2020
+// Connects to a PRSServer and runs TC1 through TC6
+// Assumes the PRSServer is run with the following command line arguments:
+//     PRSServer.exe -p 30000 -s 40000 -e 40100 -t 10
+//
+
+using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using PRSLib;
+
+namespace PRSTestClient
+{
+    class PRSTestClientProgram
+    {
+        static void Usage()
+        {
+            Console.WriteLine("usage: PRSTestClient [options]");
+            Console.WriteLine("\t-prs <serverIP>:<serverPort>");
+        }
+
+        static void Main(string[] args)
+        {
+            // defaults
+            string SERVER_IP = "127.0.0.1";
+            int SERVER_PORT = 30000;
+
+            // TODO: process command options
+            
+
+            // tell user what we're doing
+            Console.WriteLine("Test Client started...");
+            Console.WriteLine("  ServerIP = " + SERVER_IP.ToString());
+            Console.WriteLine("  ServerPort = " + SERVER_PORT.ToString());
+
+            // recommend proper PRSServer command line arguments
+            Console.WriteLine();
+            Console.WriteLine("Assumes the PRSServer is running with the following command line arguments:");
+            Console.WriteLine("    PRSServer.exe -p 30000 -s 40000 -e 40100 -t 10");
+            Console.WriteLine();
+
+            // create the socket for sending messages to the server
+            Socket ClientSocket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+
+            // construct the server's address and port
+            IPEndPoint severEP = new IPEndPoint(IPAddress.Parse(SERVER_IP), SERVER_PORT);
+
+            //
+            // Implement test cases
+            //
+
+            try
+            {
+                // call each test case method
+                TestCase1(ClientSocket, severEP);
+                TestCase2(ClientSocket, severEP);
+                TestCase3(ClientSocket, severEP);
+                TestCase4(ClientSocket, severEP);
+                TestCase5(ClientSocket, severEP);
+                TestCase6(ClientSocket, severEP);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            // close the client socket and quit
+            ClientSocket.Close();
+
+            // wait for a keypress from the user before closing the console window
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadKey();
+        }
+
+        private static void SendMessage(Socket clientSocket, IPEndPoint endPt, PRSMessage msg)
+        {
+            msg.SendMessage(clientSocket, endPt);
+        }
+
+        private static PRSMessage ExpectMessage(Socket clientSocket, string expectedMessage)
+        {
+            // receive message and validate that expected PRSMessage was received
+            EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+            PRSMessage msg = PRSMessage.ReceiveMessage(clientSocket, ref remoteEp);
+            if (msg.ToString() != expectedMessage)
+                throw new Exception("Test failed! Expected " + expectedMessage);
+
+            return msg;
+        }
+
+        private static void TestCase1(Socket clientSocket, IPEndPoint endPt)
+        {
+
+            // Simulates a PRS client, SVC1, that requests a port, keeps it alive and then closes it.
+
+            Console.WriteLine("TestCase 1 Started...");
+            //{REQUEST_PORT, “SVC1”, 0, 0}
+
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC1", 0,0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.KEEP_ALIVE, "SVC1", 40000, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.CLOSE_PORT, "SVC1", 40000, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            Console.WriteLine("TestCase 1 Passed!");
+            Console.WriteLine();
+        }
+
+        private static void TestCase2(Socket clientSocket, IPEndPoint endPt)
+        {
+            // Simulates two PRS clients, SVC1 and C1, where SVC1 requests a port, and C1 looks up the port.
+
+            Console.WriteLine("TestCase 2 Started...");
+
+            // See test cases doc
+
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC1", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.LOOKUP_PORT, "SVC1", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.CLOSE_PORT, "SVC1", 40000, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            Console.WriteLine("TestCase 2 Passed!");
+            Console.WriteLine();
+        }
+
+        private static void TestCase3(Socket clientSocket, IPEndPoint endPt)
+        {
+            // Simulates two PRS clients, SVC1 and SVC2, where SVC1 requests a port, then SVC2 requests a port and receives its own port.
+
+            Console.WriteLine("TestCase 3 Started...");
+
+            // See test cases doc
+            //send and receive service 1
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC1", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            //send and receive service 2
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC2", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC2, 40001, SUCCESS}");
+
+            //close port 1
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.CLOSE_PORT, "SVC1", 40000, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            //close port 2
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.CLOSE_PORT, "SVC2", 40001, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC2, 40001, SUCCESS}");
+
+            Console.WriteLine("TestCase 3 Passed!");
+            Console.WriteLine();
+        }
+
+        private static void TestCase4(Socket clientSocket, IPEndPoint endPt)
+        {
+            Console.WriteLine("TestCase 4 Started...");
+            // Simulates two PRS clients, SVC1 and SVC2, where SVC1 requests a port, SVC1 fails to keep the port alive, then SVC2 requests a port and receives SVC1’s expired port.
+            
+            //send and receive service 1
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC1", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+            Console.WriteLine("Please wait. Sleeping for 15 sec");
+            Thread.Sleep(15000);
+
+            //send and receive service 2
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC2", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC2, 40000, SUCCESS}");
+
+            //close port 2
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.CLOSE_PORT, "SVC2", 40000, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC2, 40000, SUCCESS}");
+
+
+            Console.WriteLine("TestCase 4 Passed!");
+            Console.WriteLine();
+        }
+
+        private static void TestCase5(Socket clientSocket, IPEndPoint endPt)
+        {
+            // Simulates two PRS clients, SVC1 and SVC2, where SVC1 requests a port, SVC1 keeps the port alive, then SVC2 requests a port and receives its own port.
+
+            Console.WriteLine("TestCase 5 Started...");
+
+            // Simulates two PRS clients, SVC1 and SVC2, where SVC1 requests a port, SVC1 keeps the port alive, then SVC2 requests a port and receives its own port.
+
+            //send and receive service 1
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC1", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            //sleep
+            Console.WriteLine("Please wait. Sleeping for 8 sec");
+            Thread.Sleep(8000);
+            
+            //keep service 1 alive
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.KEEP_ALIVE, "SVC1", 40000, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+
+            //sleep
+            Console.WriteLine("Please wait. Sleeping for 8 sec");
+            Thread.Sleep(8000);
+            
+            //send and receive service 2
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.REQUEST_PORT, "SVC2", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC2, 40001, SUCCESS}");
+
+            //close port 2
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.CLOSE_PORT, "SVC1", 40000, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, SVC1, 40000, SUCCESS}");
+
+            Console.WriteLine("TestCase 5 Passed!");
+            Console.WriteLine();
+        }
+
+        private static void TestCase6(Socket clientSocket, IPEndPoint endPt)
+        {
+            // Simulates a PRS client, M, that tells the PRS to stop
+
+            Console.WriteLine("TestCase 6 Started...");
+
+            //Stop PRS server
+            SendMessage(clientSocket, endPt, new PRSMessage(PRSMessage.MESSAGE_TYPE.STOP, "", 0, 0));
+            ExpectMessage(clientSocket, "{RESPONSE, , 0, SUCCESS}");
+
+            Console.WriteLine("TestCase 6 Passed!");
+            Console.WriteLine();
+        }
+    }
+
+}
